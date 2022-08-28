aafa5f13d08ec2098c0269518ffb501a14e94079
iamfemiab <abitogun22@gmail.com>
2022-08-28 15:24:16 +0100

    added enabled cloudoniries for other subjects

--- controller/MarketingDepartment/monthlyContribution.js
+++ controller/MarketingDepartment/monthlyContribution.js
@@ -41,217 +41,207 @@ const getSuscriberAccountById = async (req, res) => {
 
 // CREATE USER
 const createSuscriberAccount = async (req, res) => {
+
     try {
-        let result = await cloudinary.uploader.upload(req.files.customerImage[0].path);  
-        console.log(result);
-        res.send("ok");
+        let referee1ImagePath = null;
+        let referee2ImagePath = null;
+        let customerImagePath = null;
         
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({error})
+        if (req.files.customerImage) {
+            // console.log(req.files.customerImage)
+            let result = await cloudinary.uploader.upload(req.files.customerImage[0].path);
+            customerImagePath = req.files.customerImage[0].path;
+            console.log(result);
+        }
+
+        if (req.files.referee1Image) {
+            let result2 = await cloudinary.uploader.upload(req.files.referee1Image[0].path);
+            referee1ImagePath = req.files.referee1Image[0].path;
+            console.log(result2);
+        }
+
+        if (req.files.referee2Image) {
+            let result3 = await cloudinary.uploader.upload(req.files.referee2Image[0].path);
+            referee2ImagePath = req.files.referee2Image[0].path;
+            console.log(result3);
+        }
+
+        // const {
+        //     referalCode,
+        //     branch,
+        //     formNo,
+        //     state,
+        //     unitCode,
+
+        //     fullName,
+        //     residentialAddress,
+        //     email,
+        //     phone,
+        //     occupation,
+        //     maritalStatus,
+        //     religion,
+        //     gender,
+        //     birthday,
+        //     permanentHomeAddress,
+        //     stateOfOrigin,
+        //     LGA,
+        //     homeTown,
+        //     prefferDaysOfMeeting,
+        //     contributionPlan,
+        //     bankName,
+        //     accountNumber,
+        //     BVN,
+        //     meansOfIdentification,
+        //     idCardNo,
+        //     kinFullname,
+        //     kinAddress,
+        //     kinEmail,
+        //     kinPhone,
+        //     kinOccupation,
+        //     kinOfficeAddress,
+        //     kinRelationshipType,
+        //     kinYearOfrelationship,
+
+
+        //     referee1FullName,
+        //     referee1HomeAddress,
+        //     referee1WorkAddress,
+        //     referee1Business,
+        //     referee1Email,
+        //     referee1Religion,
+        //     referee1Phone,
+        //     referee1Relationship,
+
+        //     referee2FullName,
+        //     referee2HomeAddress,
+        //     referee2WorkAddress,
+        //     referee2Business,
+        //     referee2Email,
+        //     referee2Religion,
+        //     referee2Phone,
+        //     referee2Relationship
+
+
+        // } = req.body;
+
     }
-   
-    // try {
+        //     newUserSuscriber = new Suscriber({
 
-    //     let referee1ImagePath = null;
-    //     let referee2ImagePath = null;
-    //     let customerImagePath = null;
+        //         customerImagePath,
+        //         referee1ImagePath,
+        //         referee2ImagePath,
 
-    //     // console.log(req.files)
-
-    //     if (req.files.customerImage) {
-    //         // console.log(req.files.customerImage)
-    //         customerImagePath = req.files.customerImage[0].path;
-         
-    //     }
-
-    //     if (req.files.referee1Image) {
-    //         // await cloudinary.uploader.upload(req.files.referee1Image);
-    //         referee1ImagePath = req.files.referee1Image[0].path;
-    //     }
-
-    //     if (req.files.referee2Image) {
-    //         //await cloudinary.uploader.upload(req.files.referee2Image);
-    //         referee2ImagePath = req.files.referee2Image[0].path;
-    //     }
-
-    //     const {
-    //         referalCode,
-    //         branch,
-    //         formNo,
-    //         state,
-    //         unitCode,
-
-    //         fullName,
-    //         residentialAddress,
-    //         email,
-    //         phone,
-    //         occupation,
-    //         maritalStatus,
-    //         religion,
-    //         gender,
-    //         birthday,
-    //         permanentHomeAddress,
-    //         stateOfOrigin,
-    //         LGA,
-    //         homeTown,
-    //         prefferDaysOfMeeting,
-    //         contributionPlan,
-    //         bankName,
-    //         accountNumber,
-    //         BVN,
-    //         meansOfIdentification,
-    //         idCardNo,
-    //         kinFullname,
-    //         kinAddress,
-    //         kinEmail,
-    //         kinPhone,
-    //         kinOccupation,
-    //         kinOfficeAddress,
-    //         kinRelationshipType,
-    //         kinYearOfrelationship,
+        //         referalCode,
+        //         state,
+        //         branch,
+        //         formNo,
+        //         unitCode,
 
 
-    //         referee1FullName,
-    //         referee1HomeAddress,
-    //         referee1WorkAddress,
-    //         referee1Business,
-    //         referee1Email,
-    //         referee1Religion,
-    //         referee1Phone,
-    //         referee1Relationship,
+        //         fullName,
+        //         residentialAddress,
+        //         email,
+        //         phone,
+        //         occupation,
+        //         maritalStatus,
+        //         religion,
+        //         gender,
+        //         birthday,
+        //         permanentHomeAddress,
+        //         stateOfOrigin,
+        //         LGA,
+        //         homeTown,
 
-    //         referee2FullName,
-    //         referee2HomeAddress,
-    //         referee2WorkAddress,
-    //         referee2Business,
-    //         referee2Email,
-    //         referee2Religion,
-    //         referee2Phone,
-    //         referee2Relationship
+        //         prefferDaysOfMeeting,
+        //         contributionPlan,
+        //         bankName,
+        //         accountNumber,
+        //         BVN,
+        //         meansOfIdentification,
+        //         idCardNo,
+        //         kinFullname,
+        //         kinAddress,
+        //         kinEmail,
+        //         kinPhone,
+        //         kinOccupation,
+        //         kinOfficeAddress,
+        //         kinRelationshipType,
+        //         kinYearOfrelationship,
 
+        //         referee1FullName,
+        //         referee1HomeAddress,
+        //         referee1WorkAddress,
+        //         referee1Business,
+        //         referee1Email,
+        //         referee1Religion,
+        //         referee1Phone,
+        //         referee1Relationship,
 
-    //     } = req.body;
+        //         referee2FullName,
+        //         referee2HomeAddress,
+        //         referee2WorkAddress,
+        //         referee2Business,
+        //         referee2Email,
+        //         referee2Religion,
+        //         referee2Phone,
+        //         referee2Relationship
 
+        //     })
+        //     await newUserSuscriber.save();
+        //     return res.status(201).json("saved successfully");
+        // } //const createSuscriberAccount();
 
-    //     newUserSuscriber = new Suscriber({
+        catch (err) {
+            res.status(500).json({
+                errorMessage: err.message
+            })
+            //console.log(err.message)
+        }
+    }
 
-    //         customerImagePath,
-    //         referee1ImagePath,
-    //         referee2ImagePath,
-
-    //         referalCode,
-    //         state,
-    //         branch,
-    //         formNo,
-    //         unitCode,
-
-
-    //         fullName,
-    //         residentialAddress,
-    //         email,
-    //         phone,
-    //         occupation,
-    //         maritalStatus,
-    //         religion,
-    //         gender,
-    //         birthday,
-    //         permanentHomeAddress,
-    //         stateOfOrigin,
-    //         LGA,
-    //         homeTown,
-
-    //         prefferDaysOfMeeting,
-    //         contributionPlan,
-    //         bankName,
-    //         accountNumber,
-    //         BVN,
-    //         meansOfIdentification,
-    //         idCardNo,
-    //         kinFullname,
-    //         kinAddress,
-    //         kinEmail,
-    //         kinPhone,
-    //         kinOccupation,
-    //         kinOfficeAddress,
-    //         kinRelationshipType,
-    //         kinYearOfrelationship,
-
-    //         referee1FullName,
-    //         referee1HomeAddress,
-    //         referee1WorkAddress,
-    //         referee1Business,
-    //         referee1Email,
-    //         referee1Religion,
-    //         referee1Phone,
-    //         referee1Relationship,
-
-    //         referee2FullName,
-    //         referee2HomeAddress,
-    //         referee2WorkAddress,
-    //         referee2Business,
-    //         referee2Email,
-    //         referee2Religion,
-    //         referee2Phone,
-    //         referee2Relationship
-
-    //     })
-    //     await newUserSuscriber.save();
-    //     return res.status(201).json("saved successfully");
-    // } //const createSuscriberAccount();
-
-    // catch (err) {
-    //     res.status(500).json({
-    //         errorMessage: err.message
-    //     })
-
-    //     //console.log(err.message)
-    // }
-
-}
 
 
 // DELETE USERS
 const deleteSuscriberAccount = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const user = await Suscriber.findByIdAndDelete(id)
+        try {
+            const id = req.params.id;
+            const user = await Suscriber.findByIdAndDelete(id)
 
-        if (user.customerImagePath !== null) {
-            fs.rmSync(user.customerImagePath);
+            if (user.customerImagePath !== null) {
+                fs.rmSync(user.customerImagePath);
+            }
+            if (user.referee1ImagePath !== null) {
+                fs.rmSync(user.referee1ImagePath);
+            }
+            if (user.referee2ImagePath !== null) {
+                fs.rmSync(user.referee2ImagePath);
+            }
+
+
+
+
+
+
+
+
+            return res.status(200).json(user)
         }
-        if (user.referee1ImagePath !== null) {
-            fs.rmSync(user.referee1ImagePath);
+        catch (err) {
+            res.status(500).json({
+                errorMessage: " failed to delete user from database...."
+            })
         }
-        if (user.referee2ImagePath !== null) {
-            fs.rmSync(user.referee2ImagePath);
-        }
 
-
-
-
-
-
-
-
-        return res.status(200).json(user)
     }
-    catch (err) {
-        res.status(500).json({
-            errorMessage: " failed to delete user from database...."
-        })
-    }
 
-}
+    module.exports = {
 
-module.exports = {
+        getAllSuscriberAccount,
+        createSuscriberAccount,
+        deleteSuscriberAccount,
+        getSuscriberAccountById,
 
-    getAllSuscriberAccount,
-    createSuscriberAccount,
-    deleteSuscriberAccount,
-    getSuscriberAccountById,
-
-};
+    };
 
 
 
